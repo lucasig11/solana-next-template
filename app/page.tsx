215deed4e6493ad04afdb356755a295ee74fabea
Brian Birbrayer <58790251+birbrayerb@users.noreply.github.com>
2023-12-22 14:58:54 -0500

    Updated page.tsx so it functions.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useWalletNfts } from "@/hooks/useWalletNfts";
-import { Nft } from "@metaplex-foundation/js";
-import { useWallet } from "@solana/wallet-adapter-react";
+import { Metaplex, keypairIdentity } from "@metaplex-foundation/js";
+import { ConnectionProvider, WalletProvider, useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork,WalletNotConnectedError } from '@solana/wallet-adapter-base';
 import dynamic from "next/dynamic";
-import { useState } from "react";
-
+import { useState, useEffect, useMemo } from "react";
+import { Connection, clusterApiUrl, Keypair, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
+//import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
 // Dynamically import components
 const NftSelect = dynamic(() => import("./components/NftSelect"), {
   ssr: false,
@@ -12,23 +14,107 @@ const NftSelect = dynamic(() => import("./components/NftSelect"), {
 
 export default function Home() {
   const { connected, publicKey } = useWallet();
+  const network = WalletAdapterNetwork.Mainnet;
+  //const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const connection = new Connection("https://solana-mainnet.g.alchemy.com/v2/MA272Pjoji-6ZEYlK73TG97ycsztr8kY");
+  console.log(connection);
+  const [allNFTs, setAllNFTs] = useState<any[]>([]);
+  const [allNFTImages, setAllNFTImages] = useState<any[]>([]);
   // const [publicKey, setPublicKey] = useState<PublicKey | undefined>();
-  const { nfts, loading } = useWalletNfts(publicKey || undefined);
-  const [selectedNfts, setSelectedNfts] = useState<Nft[]>([]);
+  //const { nfts, loading } = useWalletNfts(publicKey || undefined);
+  //const [selectedNfts, setSelectedNfts] = useState<Nft[]>([]);
+  //const allNFTs = await Nft.nfts().findAllByOwner({ owner: owner });
+  
+  const imageStyle = {
+    maxWidth: '100px',
+    height: 'auto'
+  };
+
+  async function fetchNFTData(uri: string) {
+    try {
+      const response = await fetch(uri);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      console.log(data.name);
+      console.log(data.image);
+      // Add the image URL to the array
+      allNFTImages.push(data.image);
+      // console.log(allNFTImages);
+      // Now you can use the 'data' object as needed in your application
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  }
+
+  useEffect(() => {
+    if (publicKey) {
+      (async () => {
+        
+        const keypair = Keypair.generate();
+        console.log(keypair);
+        const metaplex = new Metaplex(connection);
+        metaplex.use(keypairIdentity(keypair));
+  
+        const owner = new PublicKey(publicKey.toString());
+        console.log(owner);
+        const allNFTs = await metaplex.nfts().findAllByOwner({ owner });
+  
+        //console.log(allNFTs);
+        setAllNFTs(allNFTs);
+        // write a loop to fetch the data from the uri of each nft
+        let nftLength = allNFTs.length - 1;
+        //console.log(nftLength);
+        while (nftLength > 0) {
+          const nft = allNFTs[nftLength];
+          
+          if (nft) {
+            fetchNFTData(nft.uri);
+          }
+          nftLength--;
+        }
+
+        setAllNFTImages(allNFTImages);
+        console.log(allNFTImages);
+      })();
+    }
+  }, [publicKey]); // Add publicKey to the dependency array to re-run the effect when publicKey changes
+
 
   return (
     <div className="m-auto flex w-full flex-col items-center space-y-10 py-10 xl:w-1/2">
-      <a
-        href="https://github.com/lucasig11/solana-next-template/generate"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex w-full items-center justify-between gap-2 rounded-md bg-gray-800 px-4 py-2 text-xl text-sky-100"
-      >
-        <svg className="h-8 w-8 fill-sky-100" viewBox="0 0 512 512">
-          <path d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z" />
-        </svg>
-        <p className="m-auto">Use this template</p>
-      </a>
+    
+
+      <div>
+      <p>Connected: {connected ? connected.toString() : 'False'}</p>
+      <p>Public Key: {publicKey ? publicKey.toString() : 'Not available'}</p>
+    
+      </div>
+
+      <div>
+        {allNFTs && allNFTs.length > 0 ? (
+          allNFTs.map((nft_id, index) => (
+            <div key={index}>
+              {/* Display properties of each NFT object here */}
+              <p>Name: {nft_id.name}</p>
+              {/* Add more properties as needed */}
+            
+            </div>
+          ))
+        ) : (
+          <p>No NFTs found</p>
+        )}
+      </div>
+
+      <div>
+        {allNFTImages.map((url, index) => (
+          <img key={index} src={url} alt="NFT Image" style={imageStyle}/>
+        ))}
+      </div>
+
 
       {/*<div className="flex w-full rounded-md bg-gray-800 px-4 py-2 text-lg text-sky-100">
         <input
@@ -46,15 +132,19 @@ export default function Home() {
             <h1 className="text-3xl font-bold text-gray-900">NFT Select</h1>
             <NftSelect
               multiple
-              text={loading ? "Loading..." : "Select NFTs..."}
-              nfts={nfts}
-              value={selectedNfts}
-              onChange={setSelectedNfts}
-              disabled={loading || nfts.length === 0}
+              nfts={allNFTs}
+              value={allNFTs}
+              onChange={setAllNFTs}
+              //disabled={loading || nfts.length === 0}
             />
           </>
         )}
-      </div>
+        </div>
+
+        {/* write a way to grab the users nft from their wallet and display it here. */}
+        
+
+
     </div>
   );
 }
